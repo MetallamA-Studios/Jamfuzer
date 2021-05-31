44888277293574dccd8fd65d94906db07d3fd456
Jake Meaker <JM219785@falmouth.ac.uk>
2021-05-31 14:26:40 +0100

    Skeleton of Save System

---
+++ Assets/Scripts/PlayerProfile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerProfile
+{
+    public string playerName;
+    public int currency;
+    public int experienceLevel;
+}
---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    private static SaveData _current;
+    public static SaveData current
+    {
+        get
+        {
+            if (_current == null)
+            {
+                _current = new SaveData();
+            }
+
+            return _current;
+        }
+        set
+        {
+            if(value != null)
+            {
+                _current = value;
+            }
+        }
+    }
+
+    public PlayerProfile profile;
+
+
+    public void OnLoadGame()
+    {
+        current = (SaveData)SerializationManager.Load
+                       (Application.persistentDataPath + "/saves/" + current.profile.playerName + ".save");
+    }
+
+    public void OnSaveGame()
+    {
+        SerializationManager.Save(current.profile.playerName, this);
+    }
+}
