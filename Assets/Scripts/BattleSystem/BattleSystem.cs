c348f75b85646b761df457fa09ebecb05bc61edc
Jake Meaker <JM219785@falmouth.ac.uk>
2021-05-31 23:06:20 +0100

    Beginning work on the turn based combat

---
+++ Assets/Scripts/BattleSystem/BattleSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState
+{
+    Start,
+    PlayerTurn,
+    EnemyTurn,
+    PlayerWin,
+    PlayerLose
+}
+
+public class BattleSystem : MonoBehaviour
+{
+    public BattleState currentState;
+
+    
+
+    void StartCombat(PlayerProfile player, PlayerProfile[] enemies)
+    {
+        currentState = BattleState.Start;
+
+
+    }
+
+    
+
+}
