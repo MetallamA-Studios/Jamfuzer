6b0fcc9ab15c32bd2f133f87d537b130e46832b3
Jake Meaker <JM219785@falmouth.ac.uk>
2021-06-02 14:50:12 +0100

    Tooltips implemented

---
+++ Assets/Scripts/ToolTipScripts/Tooltip.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class Tooltip : MonoBehaviour
+{
+    public TextMeshProUGUI headerField;
+
+    public TextMeshProUGUI contentField;
+
+    public LayoutElement layoutElement;
+
+    public int characterWrapLimit;
+
+    public RectTransform rectTransform;
+
+    public CanvasGroup cg_canvasGroup;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void SetText(string content, string header = "")
+    {
+        if (string.IsNullOrEmpty(header))
+        {
+            headerField.gameObject.SetActive(false);
+        }
+        else
+        {
+            headerField.gameObject.SetActive(true);
+            headerField.text = header;
+        }
+
+        contentField.text = content;
+
+        int headerLength = headerField.text.Length;
+        int contentLength = contentField.text.Length;
+
+        layoutElement.enabled = (headerLength > characterWrapLimit || contentLength > characterWrapLimit) ? true : false;
+    }
+
+    private void Update()
+    {
+        if (Application.isEditor)
+        {
+            int headerLength = headerField.text.Length;
+            int contentLength = contentField.text.Length;
+
+            layoutElement.enabled = (headerLength > characterWrapLimit || contentLength > characterWrapLimit) ? true : false;
+        }
+
+        Vector2 v2_mousePosition = Input.mousePosition;
+
+        float f_pivotX = v2_mousePosition.x / Screen.width;
+        float f_pivotY = v2_mousePosition.y / Screen.height;
+
+
+        rectTransform.pivot = new Vector2(f_pivotX, f_pivotY);
+        transform.position = v2_mousePosition;
+    }
+}
---
+++ Assets/Scripts/ToolTipScripts/TooltipSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipSystem : MonoBehaviour
+{
+    private static TooltipSystem tts_current;
+
+    public Tooltip tooltip;
+
+    private void Awake()
+    {
+        tts_current = this;
+    }
+
+    private void Start()
+    {
+        tts_current.tooltip.cg_canvasGroup.alpha = 0;
+    }
+
+    public static void Show(string content, string header = "")
+    {
+        tts_current.tooltip.SetText(content, header);
+        tts_current.tooltip.cg_canvasGroup.alpha = 0;
+        tts_current.tooltip.gameObject.SetActive(true);
+        tts_current.tooltip.cg_canvasGroup.LeanAlpha(1, 0.5f).setEaseInQuart();
+    }
+
+    public static void Hide()
+    {
+        tts_current.tooltip.cg_canvasGroup.LeanAlpha(0, 0.3f).setEaseOutQuart().setOnComplete(tts_current.On_Complete);
+    }
+
+    void On_Complete()
+    {
+        tts_current.tooltip.gameObject.SetActive(false);
+    }
+}
