8d219496f95bb4fe4aa0d80e4e99de0395410083
JEMMA GILBERT <jg218537@falmouth.ac.uk>
2021-06-02 17:55:30 +0100

    Added:
    - UI Prefab.
    - FollowMouse Script for Kim.
    
    Signed-off-by: JEMMA GILBERT <jg218537@falmouth.ac.uk>

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition = new Vector2(mousePosition.x, mousePosition.y);
+
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+
+    }
+}
