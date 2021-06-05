7c30d8a2abfd601f216e1dc624c8ee0a38839565
JEMMA GILBERT <jg218537@falmouth.ac.uk>
2021-06-05 13:37:58 +0100

    Added:
    - Room Re Colour system, just drag and drop and new colour theme SO and change the prefab's
    colouring.
    - Made prefabs for objects.
    - Made prefab template room.
    - Made tiles.

---
+++ Assets/Scripts/ColouringSystem/ColourThemeSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Colour Theme", menuName = "CustomEnviro/ColourTheme", order = 11)]
+public class ColourThemeSO : ScriptableObject
+{
+    public Color mainColor = new Color(255, 255, 255, 1);
+    public Color secondaryColor = new Color(255, 255, 255, 1);
+    public Color thirdColor = new Color(255, 255, 255, 1);
+    public Color lightColor = new Color(255, 255, 255, 1);
+    public Gradient gradientLightColor;
+    public Gradient gradientSecondaryColor;
+}
---
+++ Assets/Scripts/ColouringSystem/ReColourRoomScript.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Tilemaps;
+
+[ExecuteInEditMode]
+public class ReColourRoomScript : MonoBehaviour
+{
+    public ColourThemeSO currentColourTheme;
+    public GameObject room;
+
+    public bool useGradientLights;
+    public bool useGradientSecondary;
+
+    public void ReColour()
+    {
+        foreach(Transform child in room.transform)
+        {
+
+            switch (child.name)
+            {
+                case string a when a.Contains("Objects"):
+
+                    foreach (Transform childOfChild in child)
+                    {
+                        switch (childOfChild.name)
+                        {
+                            case string d when d.Contains("Decal"):
+
+                                switch (useGradientLights)
+                                {
+                                    case true:
+                                        Color randColor = currentColourTheme.gradientLightColor.Evaluate(Random.Range(0f, 1f));
+                                        childOfChild.GetComponent<SpriteRenderer>().color = randColor;
+                                        break;
+
+                                    case false:
+                                        childOfChild.GetComponent<SpriteRenderer>().color = currentColourTheme.lightColor;
+                                        break;
+                                }
+                                break;
+
+                            case string e when e.Contains("Pillar"):
+
+                                switch (useGradientSecondary)
+                                {
+                                    case true:
+                                        Color randColor = currentColourTheme.gradientSecondaryColor.Evaluate(Random.Range(0f, 1f));
+                                        childOfChild.GetComponent<SpriteRenderer>().color = randColor;
+                                        break;
+
+                                    case false:
+                                        childOfChild.GetComponent<SpriteRenderer>().color = currentColourTheme.secondaryColor;
+                                        break;
+                                }
+
+                                switch (useGradientLights)
+                                {
+                                    case true:
+                                        Color randColor = currentColourTheme.gradientLightColor.Evaluate(Random.Range(0f, 1f));
+                                        childOfChild.Find("Decal").transform.GetComponent<SpriteRenderer>().color = randColor;
+                                        break;
+
+                                    case false:
+                                        childOfChild.Find("Decal").transform.GetComponent<SpriteRenderer>().color = currentColourTheme.lightColor;
+                                        break;
+                                }
+                                break;
+
+                            case string f when f.Contains("Door"):
+
+                                switch (useGradientSecondary)
+                                {
+                                    case true:
+                                        Color randColor = currentColourTheme.gradientSecondaryColor.Evaluate(Random.Range(0f, 1f));
+                                        childOfChild.GetComponent<SpriteRenderer>().color = randColor;
+                                        break;
+
+                                    case false:
+                                        childOfChild.GetComponent<SpriteRenderer>().color = currentColourTheme.secondaryColor;
+                                        break;
+                                }
+
+                                switch (useGradientLights)
+                                {
+                                    case true:
+                                        Color randColor = currentColourTheme.gradientLightColor.Evaluate(Random.Range(0f, 1f));
+                                        childOfChild.Find("Decal").transform.GetComponent<SpriteRenderer>().color = randColor;
+                                        break;
+
+                                    case false:
+                                        childOfChild.Find("Decal").transform.GetComponent<SpriteRenderer>().color = currentColourTheme.lightColor;
+                                        break;
+                                }
+                                break;
+                        }
+                    }
+                    break;
+
+                case string b when b.Contains("Base"):
+                    child.GetComponent<Tilemap>().color = currentColourTheme.mainColor;
+                    break;
+
+                case string c when c.Contains("Top"):
+                    child.GetComponent<Tilemap>().color = currentColourTheme.thirdColor;
+                    break;
+
+                case string g when g.Contains("Wall"):
+                    child.GetComponent<Tilemap>().color = currentColourTheme.thirdColor;
+                    break;
+
+            }
+        }
+    }
+}
