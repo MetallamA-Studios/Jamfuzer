9af95af34521655e2ea5279954cc8eef80c9f4d6
JEMMA GILBERT <jg218537@falmouth.ac.uk>
2021-06-30 15:53:42 +0100

    Added:
    - Animations for Attack Actions. (Sprites, Animations + Controller.)
    - Test Script to showcase these animations.
    - Edits to my scene with this stuff.
    
    Signed-off-by: JEMMA GILBERT <jg218537@falmouth.ac.uk>

---
+++ Assets/Scripts/TempActionScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempActionScript : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer sr;
+
+    void OnMouseOver()
+    {
+        animator.SetBool("IsHovering", true);
+    }
+
+    void OnMouseDown()
+    {
+        animator.SetTrigger("Clicked");
+    }
+
+    void OnMouseExit()
+    {
+        animator.SetBool("IsHovering", false);
+    }
+}
